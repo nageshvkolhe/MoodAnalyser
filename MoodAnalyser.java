96ea96d29ea3c3b122bc054c43a0d46ad832b08d
gitbash <kolhenagesh2017@gmail.com>
2021-05-30 20:01:02 +0530

    Added : MoodAnalyser

--- MoodAnalyser.java
+++ MoodAnalyser.java
@@ -1,5 +1,12 @@
-class MoodAnalyser {
-   public static void main(String[] args){
-      System.out.println("Welcome");
-   }
+package com.MoodAnalyser;
+
+public class MoodAnalyser {
+	public String analyseMood(String message) {
+      if(message.contains("Sad")) {
+           return "SAD";
+      }     
+      else 
+    	   return "Happy";
+	}   
 }
+
